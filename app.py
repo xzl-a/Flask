09a30b7049e8a0cb29f2f2c96a055b2f03e75b90
xzl-a <2430351260@qq.com>
2023-03-31 09:39:38 -0400

    Create, edit and delete item by form

--- app.py
+++ app.py
@@ -33,7 +33,14 @@ class Movie(db.Model):  # 表名将会是 movie
 
 
 
-
+@app.cli.command()  # 注册为命令，可以传入 name 参数来自定义命令
+@click.option('--drop', is_flag=True, help='Create after drop.')  # 设置选项
+def initdb(drop):
+    """Initialize the database."""
+    if drop:  # 判断是否输入了选项
+        db.drop_all()
+    db.create_all()
+    click.echo('Initialized database.')  # 输出提示信息
 
 
 @app.cli.command()
